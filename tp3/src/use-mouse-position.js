9576a3521b98392042a1f3ceebe98de9c96c0e60
Morgan Guimard <morgan.guimard@docdoku.com>
2025-03-26 13:50:13 +0100

    tp3 review

--- tp3/src/use-mouse-position.js
+++ tp3/src/use-mouse-position.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 function useMousePosition() {
     let [mouse, setMouse] = useState({ x: 0, y: 0 })
   
+    let handleMouseMove = useCallback((e) => {
+      setMouse({ x: e.clientX, y: e.clientY })
+    },[])
+
     useEffect(() => {
-      window.addEventListener('mousemove', (e) => {
-        setMouse({ x: e.clientX, y: e.clientY })
-      })
-      return () => {
-        // remove event listener
+      window.addEventListener('mousemove',handleMouseMove)
+      return () => {        
+        window.removeEventListener('mousemove',handleMouseMove)
       }
-    }, [])
+    }, [handleMouseMove])
   
     return mouse;
   }
--- tp3/src/use-timer.js
+++ tp3/src/use-timer.js
@@ -1,15 +1,16 @@
 import { useEffect, useRef, useState } from "react"
 
 function useTimer(timeout = 200, autostart = true) {
-    let [count, setcount] = useState(0)
+    let [count, setCount] = useState(0)
     let [started, setStarted] = useState(autostart)
     let intervalRef = useRef(null)
   
     useEffect(() => {
       if (started) {
-        intervalRef.current = setInterval(() => setcount((c) => c + 1), timeout)
+        intervalRef.current = setInterval(() => setCount((c) => c + 1), timeout)
       } else if (intervalRef.current) {
         clearInterval(intervalRef.current)
+        intervalRef.current = null
       }
   
       return () => {
