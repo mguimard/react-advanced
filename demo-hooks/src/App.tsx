910f3ada3eb39d2588028e432a8b7a2e11037f92
Morgan Guimard <morgan.guimard@docdoku.com>
2025-04-16 10:57:16 +0200

    hooks

--- demo-hooks/src/App.tsx
+++ demo-hooks/src/App.tsx
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import "./App.css";
 import useCounter from "./useCounter";
 import useLocalStorage from "./useLocalStorage";
@@ -23,9 +24,63 @@ function Counter() {
   );
 }
 
+enum PizzaActions {
+  AddStock,
+  RemoveStock,
+  EatAll,
+}
+
+function pizzaReducer(state, action) {
+  console.log("pizzaReducer", state, action);
+
+  switch (action) {
+    case PizzaActions.AddStock:
+      console.log("Add to stock");
+      return { ...state, stock: state.stock + 1 };
+      break;
+    case PizzaActions.RemoveStock:
+      console.log("Add to stock");
+      return { ...state, stock: state.stock - 1 };
+      break;
+    case PizzaActions.EatAll:
+      console.log("Add to stock");
+      return { ...state, stock: 0 };
+      break;
+
+    default:
+      if (action.type === "fetch") {
+        // .....
+        console.log(`fetching pizzas from ${action.server}....`);
+      }
+      break;
+  }
+
+  return state;
+}
+
+function Pizza() {
+  const [pizza, dispatch] = useReducer(pizzaReducer, { name: "chorizo", stock: 10 });
+
+  return (
+    <>
+      <p>
+        Pizza: {pizza.name}, stock: {pizza.stock}
+      </p>
+      <button onClick={() => dispatch(PizzaActions.AddStock)}>Add stock</button>
+      <button onClick={() => dispatch(PizzaActions.EatAll)}>Eat all</button>
+      <button onClick={() => dispatch({ type: "fetch", server: "server1.net" })}>
+        {" "}
+        Fetch pizzas
+      </button>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
+      <Pizza />
+      <hr />
       <ComponentA />
       <Counter />
       <Counter />
