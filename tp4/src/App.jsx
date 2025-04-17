d18fac0608f6e2982b923c0362815069a18657c0
Morgan Guimard <morgan.guimard@docdoku.com>
2025-04-17 12:22:04 +0200

    tp4 update

--- tp4/src/App.jsx
+++ tp4/src/App.jsx
@@ -3,28 +3,36 @@ import { store } from "./redux/store";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { randomizeUsers, resetUsers, updateUser } from "./redux/usersSlice";
 import { randomizeLEDs, resetLEDs, updateLed } from "./redux/ledsSlice";
+import { memo } from "react";
+
+const User = memo(({ name, status, id }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li>
+      {name}
+      <select
+        value={status}
+        onChange={(e) => {
+          dispatch(updateUser({ status: e.target.value, id }));
+        }}>
+        <option value="online">online</option>
+        <option value="busy">busy</option>
+        <option value="absent">absent</option>
+        <option value="offline">offline</option>
+      </select>
+    </li>
+  );
+});
 
 function Users() {
   const users = useSelector((state) => state.users.list);
-  const dispatch = useDispatch();
   return (
     <>
       <p>Users</p>
       <ul>
         {users.map((user) => (
-          <li key={user.ledId}>
-            {user.name}
-            <select
-              value={user.status}
-              onChange={(e) => {
-                dispatch(updateUser({ status: e.target.value, id: user.ledId }));
-              }}>
-              <option value="online">online</option>
-              <option value="busy">busy</option>
-              <option value="absent">absent</option>
-              <option value="offline">offline</option>
-            </select>
-          </li>
+          <User key={user.ledId} name={user.name} id={user.ledId} status={user.status} />
         ))}
       </ul>
     </>
